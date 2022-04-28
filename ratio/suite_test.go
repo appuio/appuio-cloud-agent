2fbe52837bed9e3d03b57d17a83225a2b6a2326f
Fabian Fischer <glrf@users.noreply.github.com>
2022-04-28 14:30:24 +0200

    Add tests for fetcher

--- ratio/suite_test.go
+++ ratio/suite_test.go
@@ -1,11 +1,19 @@
 package ratio
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
 func assertResourceEqual(t *testing.T, res *resource.Quantity, s string) bool {
@@ -29,6 +37,58 @@ func newTestContainers(res []containerResources) []corev1.Container {
 		containers = append(containers, container)
 	}
 	return containers
+}
+
+func testNamespace(name string) *corev1.Namespace {
+	return &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+}
+
+type failingClient struct {
+	client.WithWatch
+}
+
+func (c failingClient) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
+	lo := &client.ListOptions{}
+	for _, o := range opts {
+		o.ApplyToList(lo)
+	}
+	if strings.HasPrefix(lo.Namespace, "fail-") {
+		return apierrors.NewInternalError(errors.New("ups"))
+	}
+	return c.WithWatch.List(ctx, list, opts...)
+}
+
+func podFromResources(name, namespace string, res podResource) *corev1.Pod {
+	p := corev1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	for i, cr := range res {
+		c := corev1.Container{
+			Name: fmt.Sprintf("container-%d", i),
+			Resources: corev1.ResourceRequirements{
+				Requests: map[corev1.ResourceName]resource.Quantity{},
+			},
+		}
+		if cr.cpu != "" {
+			c.Resources.Requests[corev1.ResourceCPU] = resource.MustParse(cr.cpu)
+		}
+		if cr.memory != "" {
+			c.Resources.Requests[corev1.ResourceMemory] = resource.MustParse(cr.memory)
+		}
+		p.Spec.Containers = append(p.Spec.Containers, c)
+	}
+	return &p
 }
 
 type deployResource struct {
